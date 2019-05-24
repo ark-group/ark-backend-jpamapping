19f7a71230676756b7b9c5d82ab1496e8411ddc3
krrathore1990 <karanr15.rathore@gmail.com>
2019-05-24 14:59:37 +0530

    first commit

---
+++ src/main/java/com/arkbackendjpamapping/ArkBackendJpamappingApplication.java
@@ -0,0 +1,13 @@
+package com.arkbackendjpamapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkBackendJpamappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkBackendJpamappingApplication.class, args);
+	}
+
+}
